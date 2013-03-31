69226e23242f8c3c2d4554ae7720adcc0f9f5260
Laurent Peuch <cortex@worlddomination.be>
2013-03-31 18:18:22 +0200

    [doc] fix type in agplv3+ headers

--- django_parltrack_meps/management/commands/update_meps.py
+++ django_parltrack_meps/management/commands/update_meps.py
@@ -2,7 +2,7 @@
 
 # This file is part of django-parltrack-meps.
 #
-# Foobar is free software: you can redistribute it and/or modify
+# django-parltrack-meps is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of
 # the License, or any later version.
